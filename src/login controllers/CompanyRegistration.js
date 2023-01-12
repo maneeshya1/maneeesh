4cef32e6ce21e87c778689f8a4431d6d806ba03b
maneeshya1 <119657674+maneeshya1@users.noreply.github.com>
2023-01-12 12:43:35 +0530

    Add files via upload

---
+++ src/login controllers/CompanyRegistration.js
@@ -0,0 +1,83 @@
+const dbConn = require('../../config/db.config').promise();
+
+exports.CompanyRegistration = async (req, res, next) => {
+  
+
+  
+
+  try {
+    const [row] = await dbConn.execute(
+      // "SELECT * FROM `users` WHERE `Email`=?",
+      "SELECT * FROM `invite_users` WHERE `UserId`=?",
+
+      [req.body.UserId]
+    
+    );
+
+    if (row.length === 0) {
+      // return res.status(422).json({
+      return res.json({
+        message: "Invalid UserId",
+      });
+    }
+const [rows] =await dbConn.execute(
+  
+   'insert into company_ragistration (`company_Id`,`companyName`, `companyEmail`,`companyURL`,`remark`,`companyLocation`, `userid`,`isactive`) values(?,?,?,?,?,?,?,?)',
+  //"insert into company_ragistration (`company_Id`,`companyName`, `companyEmail`,`companyURL`,`remark`,`companyLocation`, `userid`,`isactive`) values(113,'c','c','c','c','c',16,0)",
+[
+        req.body.company_Id,   
+          req.body.companyName,  
+           req.body.companyEmail, 
+            req.body.companyURL,  
+              req.body.remark,
+               req.body.companyLocation,
+               req.body.UserId,
+               req.body.isActive
+]);
+      if (rows.affectedRows === 1) {
+        return res.status(201).json({
+          success: rows,
+          message: "The user has been successfully inserted.",
+        });
+      }
+      
+    
+   
+    return res.json({
+      success: row,
+      message: "UserID matched Successfully",
+
+      
+      
+    });
+
+    
+
+
+
+// console.log(req.body)
+//     const [row1] = dbConn.execute('insert into company_ragistration (`company_Id`,`companyName`, `companyEmail`,`companyURL`,`remark`,`companyLocation`,`userid`,`isactive`) values(?,?,?,?,?,?,?,?)',
+
+//     req.body.company_Id,   
+//     req.body.companyName,  
+//     req.body.companyEmail, 
+//       req.body.companyURL,  
+//        req.body.remark,
+//         req.body.companyLocation,
+//         req.body.UserId,
+//         req.body.isactive
+//       );
+  
+//   if(row1.affectedRows===1){
+// return res.status(201).json({
+//     message:"user has been inserted successfully",
+// })
+//   }
+
+  }
+
+
+  catch (err) {
+    next(err);
+  }
+};
---
+++ src/routes/routes.js
@@ -0,0 +1,57 @@
+const router = require('express').Router();
+const {body} = require('express-validator');
+const {register} = require('../login controllers/registerController.js');
+const {login} = require('../login controllers/loginController.js');
+// const {getUser} = require('./controllers/getUserController');
+const {getUserName} = require('../login controllers/getcontroller.js');
+const { CompanyRegistration } = require('../login controllers/CompanyRegistration.js');
+//const{getAllUser}=require('../login controllers/')
+router.post('/InviteUser', 
+[
+  body('Name',"The name must be of minimum 3 characters length")
+  .notEmpty()
+  .escape()
+  .trim()
+  .isLength({ min: 3 }),
+
+  
+  
+  body('Username',"The name must be of minimum 3 characters length")
+  .notEmpty()
+  .escape()
+  .trim()
+  .isLength({ min: 3 }),
+  
+  body('Email',"Invalid email address")
+  .notEmpty()
+  .escape()
+  .trim().isEmail(),
+  
+  body("Password", "The Password must be of minimum 4 characters length")
+     .notEmpty()
+       .trim()
+    .isLength({ min: 4 }),
+ 
+],
+ register);
+
+
+
+router.post('/login',[
+  body('Email',"Invalid Email address")
+  .notEmpty()
+  .escape()
+  .trim().isEmail(),
+  body('Password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
+],login);
+
+ router.get('/getoneuser/:Email',getUserName);
+
+
+ router.post('/subs',CompanyRegistration);
+// router.get('/getoneuser',getUserName);
+
+// router.get('/getuser',getUser)
+// router.get('/getall',getAllUser)
+
+module.exports = router;
