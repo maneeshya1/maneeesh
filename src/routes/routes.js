39b2bcdba9ba9c21233806924785291b3380b2bb
AnujSoni2208 <anuj.soni@cylsys.com>
2023-02-09 17:50:52 +0530

    Scheduler API for
    insert ,
    update,
    getby id
    get all

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,70 +1,85 @@
-const router = require('express').Router();
-const {body} = require('express-validator');
-const {register} = require('../login controllers/registerController.js');
-const {login} = require('../login controllers/loginController.js');
+const router = require("express").Router();
+const { body } = require("express-validator");
+const { register } = require("../login controllers/registerController.js");
+const { login } = require("../login controllers/loginController.js");
 // const {getUser} = require('./controllers/getUserController');
-const {getUserName} = require('../login controllers/getcontroller.js');
-const { CompanyRegistration } = require('../login controllers/CompanyRegistration.js');
-const { ContactDetails } = require('../login controllers/ContactDetails.js');
-const {ContactDetailsEdit} = require('../login controllers/ContactDetails.js');
-const{UserRoles}=require('../login controllers/UserRoleMaster.js')
+const { getUserName } = require("../login controllers/getcontroller.js");
+const {
+  CompanyRegistration,
+} = require("../login controllers/CompanyRegistration.js");
+const { ContactDetails } = require("../login controllers/ContactDetails.js");
+const {
+  ContactDetailsEdit,
+} = require("../login controllers/ContactDetails.js");
+const { UserRoles } = require("../login controllers/UserRoleMaster.js");
 // const{Campaign}=require('../login controllers/UserRoleMaster.js')
-const{Campaign}=require('../login controllers/Campaign.js')
-const{Segment}=require('../login controllers/Segment.js')
+const { Campaign } = require("../login controllers/Campaign.js");
+const { Segment } = require("../login controllers/Segment.js");
+const {
+  getScheduler,
+  addScheduler,
+  getAllScheduler,
+  updateScheduler,
+} = require("../login controllers/Scheduler");
 
 //const{getAllUser}=require('../login controllers/')
-router.post('/InviteUser', 
-[
-  body('Name',"The name must be of minimum 3 characters length")
-  .notEmpty()
-  .escape()
-  .trim()
-  .isLength({ min: 3 }),
-  
-  body('Username',"The name must be of minimum 3 characters length")
-  .notEmpty()
-  .escape()
-  .trim()
-  .isLength({ min: 3 }),
-  
-  body('Email',"Invalid email address")
-  .notEmpty()
-  .escape()
-  .trim().isEmail(),
-  
-  body("Password", "The Password must be of minimum 4 characters length")
-     .notEmpty()
-       .trim()
-    .isLength({ min: 4 }),
- 
-],
- register);
+router.post(
+  "/InviteUser",
+  [
+    body("Name", "The name must be of minimum 3 characters length")
+      .notEmpty()
+      .escape()
+      .trim()
+      .isLength({ min: 3 }),
 
-router.post('/login',[
-  body('Email',"Invalid Email address")
-  .notEmpty()
-  .escape()
-  .trim().isEmail(),
-  body('Password',"The Password must be of minimum 4 characters length").notEmpty().trim().isLength({ min: 4 }),
-],login);
+    body("Username", "The name must be of minimum 3 characters length")
+      .notEmpty()
+      .escape()
+      .trim()
+      .isLength({ min: 3 }),
 
- router.get('/getoneuser/:Email',getUserName);
+    body("Email", "Invalid email address").notEmpty().escape().trim().isEmail(),
 
+    body("Password", "The Password must be of minimum 4 characters length")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 4 }),
+  ],
+  register
+);
 
- router.post('/subs',CompanyRegistration);
+router.post(
+  "/login",
+  [
+    body("Email", "Invalid Email address").notEmpty().escape().trim().isEmail(),
+    body("Password", "The Password must be of minimum 4 characters length")
+      .notEmpty()
+      .trim()
+      .isLength({ min: 4 }),
+  ],
+  login
+);
 
- router.post('/contact',ContactDetails);
- router.put('/contact/edit',ContactDetailsEdit);
+router.get("/getoneuser/:Email", getUserName);
 
+router.post("/subs", CompanyRegistration);
 
-router.post('/userRole',UserRoles);
-router.post('/Campaign',Campaign);
-router.post('/Segment',Segment); 
+router.post("/contact", ContactDetails);
+router.put("/contact/edit", ContactDetailsEdit);
+
+router.post("/userRole", UserRoles);
+router.post("/Campaign", Campaign);
+router.post("/Segment", Segment);
 // router.post('/Campaign',Campaign);
- 
+
 // router.get('/getoneuser',getUserName);
 
 // router.get('/getuser',getUser)
 // router.get('/getall',getAllUser)
+router.get("/getScheduler/Id=?", getScheduler);
+router.put("/updateScheduler/Id=?", updateScheduler);
+router.post("/addSchedule", addScheduler);
+router.get("/getAllScheduler", getAllScheduler);
+// router.get('/getScheduler/:scheduler_Id',getScheduler);
 
 module.exports = router;
